6f162de2f8583145426d3d8d17a4edfe47333b46
tioh <ohtakin99@gmail.com>
2019-08-14 11:16:07 +0900

    polyline component 수정.
    delay로 line그릴 때 drag하면 지도와 틀어짐.

--- src/components/kakaomap/Polyline.js
+++ src/components/kakaomap/Polyline.js
@@ -1,7 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { MapContext } from "./KakaoMap";
-import { interval } from "rxjs";
-import { take } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 const Polyline = props => {
   const { kakao, map } = useContext(MapContext);
@@ -11,8 +10,37 @@ const Polyline = props => {
     map
   });
 
+  const delayObservable = (path, delay) => {
+    return new Observable(observer => {
+      if (delay) {
+        map.setDraggable(false);
+        path.forEach((p, index) => {
+          setTimeout(() => {
+            observer.next(path.slice(0, index));
+            if (index + 1 === path.length) observer.complete();
+          }, delay * index);
+        });
+      } else {
+        observer.next(path);
+        observer.complete();
+      }
+    });
+  };
+
+  useEffect(() => {
+    const { options } = props;
+    const polyline = new kakao.maps.Polyline({ ...options });
+    setState({ ...state, polyline });
+    return () => {
+      polyline.setMap(null);
+    };
+  }, []);
+
   useEffect(() => {
     const { options, delay, bounds } = props;
+    const polyline = state.polyline;
+    if (polyline === null) return;
+
     const path = options.path.map(p => {
       const { lat, lng } = p;
       return new kakao.maps.LatLng(lat, lng);
@@ -27,26 +55,22 @@ const Polyline = props => {
     } else if (bounds === false) {
       map.setCenter(Object.values(path)[0]);
     }
-
-    const polyline = new kakao.maps.Polyline({ ...options });
-    if (delay) {
-      path.forEach((p, index) => {
-        const delayedTime = interval(delay * index);
-        delayedTime.pipe(take(1)).subscribe(x => {
-          polyline.setPath(path.slice(0, index));
-          polyline.setMap(map);
-        });
-      });
-    } else {
-      polyline.setPath(path);
-      polyline.setMap(map);
-    }
+    const subscription = delayObservable(path, delay).subscribe({
+      next(p) {
+        polyline.setPath(p);
+        polyline.setMap(map);
+      },
+      error(err) {},
+      complete() {
+        map.setDraggable(true);
+      }
+    });
 
     setState({ ...state, polyline });
     return () => {
-      polyline.setMap(null);
+      subscription.unsubscribe();
     };
-  }, []);
+  }, [state.polyline, props.options.path]);
 
   return null;
 };
